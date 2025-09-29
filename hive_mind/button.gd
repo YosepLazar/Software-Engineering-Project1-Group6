d168701a6d6a3b30575f0b0a091c452cdde88b91
YosepLazar <yoseplazar13@gmail.com>
2025-09-29 16:32:00 -0500

    added texture to the menu buttons and siginificant progress on story

---
+++ hive_mind/button.gd
@@ -0,0 +1,13 @@
+extends Button
+
+func _on_play_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Level1.tscn")
+
+func _on_settings_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Settings.tscn")
+
+func _on_story_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://story.tscn")
+	
+func _on_quit_buttton_pressed() -> void:
+	get_tree().quit()
